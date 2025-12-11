ddf66c41135e260eb0684ab344905ddc0eddb76d
Shubham Chhagan Ajbale <shubhamajbale9@gmail.com>
2025-12-11 18:05:56 +0530

    added

--- ProjectModule/ALB/alb-var.tf
+++ ProjectModule/ALB/alb-var.tf
@@ -1,18 +1,3 @@
-variable "image_id" {
-  default = "ami-02b8269d5e85954ef"
-}
-variable "key_pair" {
-  default = "shubhamkeypair"
-}
-variable "instance_type" {
-  default = "t3.micro"
-}
-variable "subnet_id_1" {
-  default = "subnet-0af9bf91fc70a68f7"
-}
-variable "subnet_id_2" {
-  default = "subnet-0f23be7be7c7e96a4"
-}
-variable "vpc_id" {
-  default = "vpc-044e5fbe5f20d1289"
+variable "alb_type" {
+  default = "application"
 }
